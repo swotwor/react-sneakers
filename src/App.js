1cdaf0977af759ab973dd60cf2ed3afa10a9d066
Palekha Kostantin <swotwor@gmail.com>
2021-12-15 21:29:48 +0200

    Creating a search

--- src/App.js
+++ src/App.js
@@ -4,17 +4,18 @@ import Header from './components/Header';
 import Overlay from './components/Overlay';
 const axios = require('axios').default;
 
-// const arr = [];
-
 function App() {
 	const [items, setItems] = React.useState([]);
+	const [searchValue, setSearchValue] = React.useState('');
 	const [cardOpened, setCardOpened] = React.useState(false);
 
 	React.useEffect(() => {
-		axios.get('https://61b79b0364e4a10017d18bc8.mockapi.io/shopItems').then(function (response) {
-			setItems(response.data);
-		});
+		axios.get('https://61b79b0364e4a10017d18bc8.mockapi.io/shopItems').then((response) => setItems(response.data));
 	}, []);
+
+	const onChangeSearchValue = (event) => {
+		setSearchValue(event.target.value);
+	};
 
 	return (
 		<div className='wrapper clear'>
@@ -22,18 +23,18 @@ function App() {
 			<Header onClickCard={() => setCardOpened(!cardOpened)} />
 			<div className='content p-40'>
 				<div className='d-flex align-center mb-40 justify-between'>
-					<h1>Все кроссовки</h1>
+					<h1>{searchValue ? `Поиск по запросу: '${searchValue}'` : 'Все кроссовки'}</h1>
 					<div className='search-block d-flex'>
 						<img src='img/search.svg' alt='Search' />
-						<input type='text' placeholder='Поиск...' />
+						<input onChange={onChangeSearchValue} type='text' placeholder='Поиск...' value={searchValue} />
 					</div>
 				</div>
 				<div className='d-flex flex-row flex-wrap'>
-					{items.map((item, index) => (
+					{items.map((item) => (
 						<Card
 							name={item.name}
 							price={item.price}
-							key={index}
+							key={item.id}
 							foto={item.foto}
 							onFavorite={() => console.log('ADD TO FAVORITE')}
 							onPlus={() => console.log('ADD TO CARD')}
